4e6206eaf06b285cf6dff8d859ecea7b8b25b56a
moerekh <simony@gmail.com>
2020-02-06 17:08:29 -0800

    Added js file with employment data.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
+import my_info from './simonyounesi_info.js';
 
 class Intro extends React.Component {
   render() {
     return(
-      <div>
-        <h1>
-          Hi, I'm Simon.
-          <br />
-          <small>It's a pleasure to meet you!</small>
-        </h1>
+      <div className="container">
+        <div class="row">
+          <div className="col">
+            <h1 className="my_greeting">
+              Hi, I'm { my_info.name }.
+            </h1>
+            <p>{ my_info.introduction }</p>
+          </div>
+        </div>
       </div>
     )
   }
---
+++ src/simonyounesi_info.js
@@ -0,0 +1,112 @@
+const my_info = {
+    "name": "Simon Younesi",
+    "introduction": "I'm a Frontend Engineer! I make all sorts of apps and sites using different languages and frameworks. I love boardgames and video games. For me designing things is satisfying process. I am a problem solver in a designer's brain. Lets work together and make something amazing.",
+    "employment_history": [
+        {
+            "employer": "Harvard Medical School - Gygi Lab",
+            "title": "Frontend Engineer",
+            "duration": {
+                "start": {
+                    "month": "Januray",
+                    "year": 2017
+                },
+                "end": {
+                    "month": false,
+                    "year": false
+                }
+            },
+            "duties": [
+                "Collaborated remotely with lab scientists to build requested app modules and features that allowed them to publish their findings.",
+                "Created tools that collected files and data to deliver to the user that allowed for swift paper submissions to publishers.",
+                "Created pages with arrays of charts utilizing JavaScript promise chains that allowed for dynamic rendering of additional charts for future use and publishing.",
+                "Built modules for new application functionality with PHP.",
+                "Wrote database queries and created new databases with MySQL.",
+                "Improved user experience and user tools by expanding the use of Twitter Bootstrap and JQuery.",
+                "Contributed to create and improve community coding standards.",
+                "Refactored old code to meet current standards.",
+                "Reviewed and fixed bugs using Trac.",
+                "Utilized SVN to manage the project."
+            ]
+        },
+        {
+            "employer": "TextMarks",
+            "title":"Frontend Developer",
+            "duration": {
+                "start": {
+                    "month": "November",
+                    "year":  2013
+                },
+                "end": {
+                    "month": "December",
+                    "year": 2016
+                }
+            },
+            "duties": [
+                "Helped create a detailed Kanban method of task tracking with Asana.",
+                "Improved TextMarks services and page design by introducing modern frameworks such as Twitter Bootstrap.",
+                "Created forms and form validators using JavaScript and JQuery.",
+                "Responsible for improving existing pages and creating new marketing page templates for use by the marketing team with Python Django.",
+                "Created branding guidelines for use throughout the website and other physical marketing with Adobe Creative Suite.",
+                "Managed QA documentation, meetings, and follow up reports.",
+                "Created templated emails with the marketing team in Marketo and Wufoo.",
+                "Developed blog templates and pages with Wordpress.",
+                "Utilized Mercurial to manage the project​.",
+                "Maintained community coding standards.",
+                "Worked with SASS and CSS compilers to write and contribute to site-wide style guides.",
+                "Built creative solutions to help showcase products to customers using AngularJS and TypeScript.",
+                "Managed A/B testing with Optimizely and generated reports to help present the significance of the data collected."
+            ]
+        },
+        {
+            "employer": "SMK-Link Electronics",
+            "title": "Graphic Designer / Frontend Developer",
+            "duration": {
+                "start": {
+                    "month": "November",
+                    "year": 2010
+                },
+                "end": {
+                    "month": "November",
+                    "year": 2013
+                }
+            },
+            "duties": [
+                "Instrumental in re-launching the SMK-Link & Gyration brands with new retail packaging, user guides, magazine ads, e-blasts, newsletters, websites, tradeshow graphics, and sales collateral.",
+                "Created E-Commerce site using Shopify.",
+                "Built modules and other Shopify site functionality using Shopify Liquid.",
+                "Responsible for all artwork production from sketches, to print checks, up until product shipment. Work was also picked up and reproduced by the parent companies SMK America and SMK Japan.",
+                "Introduced SVN Tortoise to help manage files and projects."
+            ]
+        },
+        {
+            "employer": "Myself",
+            "title": "Freelance Designer / Consultant / Engineer",
+            "duration": {
+                "start": {
+                    "month": false,
+                    "year": 2013
+                },
+                "end": {
+                    "month": false,
+                    "year": false
+                }
+            },
+            "duties": [
+                "Design logos, and provide digital files for them whenever requested in Adobe compatible formats using Adobe Creative Suite, Acrobat, Photoshop, Illustrator, Bridge, InDesign.",
+                "Create, design, manage, and migrate Wordpress websites.",
+                "Design custom solutions for clients based on their needs and abilities.",
+                "Build websites with Google APIs, various frameworks and technologies.",
+                "Create treatments for entertainment client presentations.",
+                "Manage personal projects using GIT."
+            ]
+        }
+    ],
+    "skills": [
+        {
+            "name": "",
+            "score": 0
+        }
+    ]
+}
+
+export default my_info;
