7204bb16a564bd28950d369d9b50298831a0936e
moerekh <simony@gmail.com>
2020-02-06 16:08:31 -0800

    Consolidated SCSS to fone file.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.scss';
+
+class Intro extends React.Component {
+  render() {
+    return(
+      <div>
+        <h1>
+          Hi, I'm Simon.
+          <br />
+          <small>It's a pleasure to meet you!</small>
+        </h1>
+      </div>
+    )
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <main></main>
+      <header></header>
+      <main>
+        <Intro></Intro>
+      </main>
       <footer></footer>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
+import './simonyounesi.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
