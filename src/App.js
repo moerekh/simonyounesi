12ee74b3338213e87089cf82d0e725f4464b03c2
moerekh <simony@gmail.com>
2020-02-13 17:07:54 -0800

    Spaced out layout.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class Intro extends React.Component {
   render() {
     return(
       <div className="container">
-        <div class="row">
+        <div className="row">
           <div className="col">
             <h1 className="my_greeting">
               Hi, I'm { my_info.name }.
@@ -26,22 +26,26 @@ class ListGroupItems extends React.Component {
         {desc}
       </li>
     ))
-    return(
-      <li className="list-group-item">
-        <span className="employer_name">{this.props.job.employer}</span>
-        <span className="job_dates">
-        {(this.props.job.duration.start.month)? this.props.job.duration.start.month + " " + this.props.job.duration.start.year : ''} 
-        {(this.props.job.duration.present)? " - Present" : 
-        (this.props.job.duration.end.month)? " - " + this.props.job.duration.end.month + " " + this.props.job.duration.end.year : ''}
-        </span>
 
-        <span className="employer_description">{this.props.job.description}</span>
+    return(
+      <div id={"job-" + this.props.job_key} className="row">
         <hr />
-        <span className="job_title">{this.props.job.title}</span>
-        <ul>
-          {duty}
-        </ul>
-      </li>
+        <div className="col">
+          <span className="employer_name">{this.props.job.employer}</span>
+          <span className="job_dates">
+          {(this.props.job.duration.start.month)? this.props.job.duration.start.month + " " + this.props.job.duration.start.year : ''} 
+          {(this.props.job.duration.present)? " - Present" : 
+          (this.props.job.duration.end.month)? " - " + this.props.job.duration.end.month + " " + this.props.job.duration.end.year : ''}
+          </span>
+
+          <span className="employer_description">{this.props.job.description}</span>
+          <hr />
+          <span className="job_title">{this.props.job.title}</span>
+          <ul>
+            {duty}
+          </ul>
+        </div>
+      </div>
     )
   }
 }
@@ -52,6 +56,7 @@ class Employment extends React.Component {
       <ListGroupItems
         key={i}
         job={job}
+        job_key={i}
       >
       </ListGroupItems>
     ))
@@ -59,12 +64,7 @@ class Employment extends React.Component {
       <div className="container">
         <div className="row">
           <div className="col">
-            <ul className="list-group">
-              <li class="list-group-item">
-                <span className="work_history_title">Work History</span>
-              </li>
-              {listgroupitems}
-            </ul>
+            {listgroupitems}
           </div>
         </div>
       </div>
@@ -76,7 +76,7 @@ function App() {
   return (
     <div className="App">
       <header></header>
-      <main>
+      <main id="fullscreen">
         <Intro></Intro>
         <Employment></Employment>
       </main>
